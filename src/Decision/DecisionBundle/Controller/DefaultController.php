b0b15fc6de2f8e477224eee08280f94762fe5d6d
Andrei Dascalu <andrei.dascalu@arobs.com>
2014-09-01 16:56:20 +0300

    replace tab by space

--- src/Decision/DecisionBundle/Controller/DefaultController.php
+++ src/Decision/DecisionBundle/Controller/DefaultController.php
@@ -15,6 +15,6 @@ class DefaultController extends Controller
 
     public function showAction() {
 
-    	return $this->render('DecisionBundle:Default:index.html.twig');
+        return $this->render('DecisionBundle:Default:index.html.twig');
     }
 }
