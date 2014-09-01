42de5983d49ef3157546ad0083835669d531aa88
andrei <andrei.dascalu.eng@gmailc.om>
2014-09-01 09:01:32 +0300

    test fix

--- src/Decision/DecisionBundle/Tests/Controller/PlayerControllerTest.php
+++ src/Decision/DecisionBundle/Tests/Controller/PlayerControllerTest.php
@@ -7,9 +7,12 @@ use Decision\DecisionBundle\Controller\PlayerController;
 
 class PlayerControllerTest extends WebTestCase
 {
-     $client = static::createClient();
+    public function testIndex()
+    {
+        $client = static::createClient();
 
-    $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/hello/Fabien');
 
-    $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() == 0);
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() == 0);
+    }
 }
--- src/Decision/DecisionBundle/Tests/Controller/TeamControllerTest.php
+++ src/Decision/DecisionBundle/Tests/Controller/TeamControllerTest.php
@@ -6,9 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TeamControllerTest extends WebTestCase
 {
-    $client = static::createClient();
+    public function testIndex()
+    {
+        $client = static::createClient();
 
-    $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/hello/Fabien');
 
-    $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() == 0);
+        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() == 0);
+    }
 }
