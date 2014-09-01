dab64c9f1ebb8b831241a15142f355a4f00c87ab
andrei <andrei.dascalu.eng@gmailc.om>
2014-09-01 08:53:41 +0300

    tests

--- src/Decision/DecisionBundle/Tests/Controller/PlayerControllerTest.php
+++ src/Decision/DecisionBundle/Tests/Controller/PlayerControllerTest.php
@@ -7,4 +7,9 @@ use Decision\DecisionBundle\Controller\PlayerController;
 
 class PlayerControllerTest extends WebTestCase
 {
+     $client = static::createClient();
+
+    $crawler = $client->request('GET', '/hello/Fabien');
+
+    $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() == 0);
 }
--- src/Decision/DecisionBundle/Tests/Controller/TeamControllerTest.php
+++ src/Decision/DecisionBundle/Tests/Controller/TeamControllerTest.php
@@ -6,4 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TeamControllerTest extends WebTestCase
 {
+    $client = static::createClient();
+
+    $crawler = $client->request('GET', '/hello/Fabien');
+
+    $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() == 0);
 }
